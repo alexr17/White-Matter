0291ea026b73f990330c5e5afb3ecc6fc115eb07
alexr17@lakesideschool.org <alexr17@lakesideschool.org>
2017-06-27 13:57:03 -0700

    2pm june 27

--- Plotter/DataPlot.js
+++ Plotter/DataPlot.js
@@ -1,25 +1,43 @@
+var fs = require('fs')
 var plotly = require('plotly')('anicolas', 'WmmMkswSq6Wwlh5FpRIc');
+
+
+var file_path = '/Users/Alex/Desktop/WhiteMatterSummer2017/Scraper/'
+
+//read the file
+if (process.argv.length <= 2) {
+  console.log("Usage: " + __filename + file_path);
+  process.exit(-1);
+}
+var path = process.argv[2];
+
+var data = fs.readFileSync(file_path + path, 'utf8')
+//console.log(data)
+
+var names = data.split(',')
+var list = []
+for (name of names) {
+  list.push({
+    args: ['visible'],
+    label: name,
+    method: 'restyle'
+  })
+}
+//console.log(list)
+
 trace1 = {
-  x: [/*data*/], 
-  y: [/*data*/], 
+  x: [4,3,2,1], 
+  y: [8,7,6,5], 
   line: {color: 'rgba(31,119,180,1)'}, 
   type: 'scatter',  
 };
 trace2 = {
-  x: [/*data*/], 
-  y: [/*data*/], 
+  x: [1,2,3,4], 
+  y: [5,6,7,8], 
   line: {color: 'rgba(255,127,14,1)'}, 
   type: 'scatter', 
-  visible: false
 };
-var list = []
-for (var ii = 0; ii < 20; ii++) {
-  list.push({
-    args: ['line.color'],
-    label: ii,
-    method: 'restyle'
-  })
-}
+
 data = [trace1, trace2];
 layout = {
   margin: {
@@ -38,22 +56,25 @@ layout = {
   xaxis: {
     autorange: true,
     showgrid: false,
-    zeroline: false,
-    showline: false,
+    zeroline: true,
+    showline: true,
     autotick: true,
-    ticks: "",
+    linecolor: '#d3d3d3',
+    mirror: "ticks",
     showticklabels: false
   },
   yaxis: {
     autorange: true,
     showgrid: false,
-    zeroline: false,
-    showline: false,
+    zeroline: true,
+    showline: true,
     autotick: true,
-    ticks: "",
+    linecolor: '#d3d3d3',
+    mirror: "ticks",
     showticklabels: false
   }
 };
+
 plotly.plot(data, {layout: layout, fileopt: "overwrite"}, function(err, msg) {
   console.log(msg);
 });
